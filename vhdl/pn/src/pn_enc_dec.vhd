ee68d3edc5cb450110faa77cbf99febbd770df0b
gotchicode <fievet.jerome@gmail.com>
2020-05-25 14:56:32 +0200

    pn - prbs23 encoder / decoder module - tested on 8 bits byte in simulation

---
+++ vhdl/pn/src/pn_enc_dec.vhd
@@ -0,0 +1,127 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+
+entity pn_enc_dec is
+    generic (
+              bit_size      : integer:=8
+    );
+ port (
+            clk             : in std_logic;
+            rst_n             : in std_logic;
+     
+            enc_data_in     : in std_logic_vector(bit_size-1 downto 0);
+            enc_data_in_en  : in std_logic;
+            enc_data_out    : out std_logic_vector(bit_size-1 downto 0);
+            enc_data_out_en : out std_logic;
+    
+            dec_data_in     : in std_logic_vector(bit_size-1 downto 0);
+            dec_data_in_en  : in std_logic;
+            dec_data_out    : out std_logic_vector(bit_size-1 downto 0);
+            dec_data_out_en : out std_logic
+ );
+end entity pn_enc_dec;
+
+architecture rtl of pn_enc_dec is
+
+signal p1_enc_data_out_en : std_logic;
+signal p1_enc_data_out    : std_logic_vector(bit_size-1 downto 0);
+
+signal p1_dec_data_out_en : std_logic;
+signal p1_dec_data_out    : std_logic_vector(bit_size-1 downto 0);
+
+begin
+
+--template_pr: process(clk, rst_n)
+--begin
+--    if rst_n='1' then
+--    elsif rising_edge(clk) then
+--    end if;
+--end process;
+
+enc_pr: process(clk, rst_n)
+variable v_tmp                : std_logic;
+variable v_out_tmp            : std_logic; 
+variable v_out                : std_logic; 
+variable v_out_enc_table      : std_logic_vector(bit_size-1 downto 0); 
+variable v_shift_reg_enc      : std_logic_vector(31 downto 1);
+begin
+    if rst_n='0' then
+        v_tmp                :='0';
+        v_out_tmp            :='0';    
+        v_out                :='0';    
+        v_out_enc_table      := (others=>'0');  
+        v_shift_reg_enc      := (others=>'0');
+        
+        p1_enc_data_out_en   <= '0';   
+        p1_enc_data_out      <= (others=>'0');     
+
+        enc_data_out_en      <= '0';
+        enc_data_out         <= (others=>'0'); 
+        
+    elsif rising_edge(clk) then
+    
+        if enc_data_in_en='1' then
+            for I in 0 to bit_size-1 loop
+                v_tmp := enc_data_in(I);
+                v_out_tmp := v_shift_reg_enc(23) xor v_shift_reg_enc(18);
+                v_out := v_tmp xor v_out_tmp;
+                v_out_enc_table(I) := v_out;
+                v_shift_reg_enc(23 downto 2) := v_shift_reg_enc(22 downto 1);
+                v_shift_reg_enc(1) := v_out;
+            end loop;
+        end if;
+        
+        p1_enc_data_out_en <= enc_data_in_en;
+        p1_enc_data_out    <= v_out_enc_table;
+        
+        enc_data_out_en <= p1_enc_data_out_en;
+        enc_data_out    <= p1_enc_data_out;
+        
+    end if;
+end process;
+
+dec_pr: process(clk, rst_n)
+variable v_tmp                : std_logic;
+variable v_out_tmp            : std_logic; 
+variable v_out                : std_logic; 
+variable v_out_dec_table      : std_logic_vector(bit_size-1 downto 0); 
+variable v_shift_reg_dec      : std_logic_vector(31 downto 1);
+begin
+    if rst_n='0' then
+        v_tmp                :='0';
+        v_out_tmp            :='0';    
+        v_out                :='0';    
+        v_out_dec_table      := (others=>'0');  
+        v_shift_reg_dec      := (others=>'0');
+        
+        p1_dec_data_out_en   <= '0';   
+        p1_dec_data_out      <= (others=>'0');     
+
+        dec_data_out_en      <= '0';
+        dec_data_out         <= (others=>'0'); 
+        
+    elsif rising_edge(clk) then
+    
+        if dec_data_in_en='1' then
+            for I in 0 to bit_size-1 loop
+                v_tmp := dec_data_in(I);
+                v_out_tmp := v_shift_reg_dec(23) xor v_shift_reg_dec(18);
+                v_out := v_tmp xor v_out_tmp;
+                v_out_dec_table(I) := v_out;
+                v_shift_reg_dec(23 downto 2) := v_shift_reg_dec(22 downto 1);
+                v_shift_reg_dec(1) := v_tmp;
+            end loop;
+        end if;
+        
+        p1_dec_data_out_en <= dec_data_in_en;
+        p1_dec_data_out    <= v_out_dec_table;
+        
+        dec_data_out_en <= p1_dec_data_out_en;
+        dec_data_out    <= p1_dec_data_out;
+        
+    end if;
+end process;
+
+end rtl;
---
+++ vhdl/pn/tb/pn_enc_dec_tb.vhd
@@ -0,0 +1,114 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity pn_enc_dec_tb is
+end pn_enc_dec_tb;
+
+architecture rtl of pn_enc_dec_tb is
+
+constant clk_const      : time := 5 ns;
+constant rst_n_const    : time := 123 ns;
+
+constant bit_size       : integer:=8;
+
+signal clk : std_logic;
+signal rst_n : std_logic;
+signal enc_data_in : std_logic_vector(bit_size-1 downto 0);
+signal enc_data_in_en : std_logic;
+signal enc_data_out : std_logic_vector(bit_size-1 downto 0);
+signal enc_data_out_en : std_logic;
+signal dec_data_in : std_logic_vector(bit_size-1 downto 0);
+signal dec_data_in_en : std_logic;
+signal dec_data_out : std_logic_vector(bit_size-1 downto 0);
+signal dec_data_out_en : std_logic;
+
+
+
+begin
+
+    -----------------------------------------
+    -- clk generation
+    -----------------------------------------
+    clk_gen_pr: process
+    begin
+        clk <= '1';
+        wait for clk_const;
+
+        clk <= '0';
+        wait for clk_const;
+    end process;
+
+
+    -----------------------------------------
+    -- rst_n generation
+    -----------------------------------------
+    rst_n_gen_pr:process
+    begin
+
+        rst_n <= '1';
+        wait for 1 us;
+
+        rst_n <= '0';
+        wait for rst_n_const;
+
+        rst_n <= '1';
+        wait;
+
+    end process;
+    
+    -----------------------------------------
+    -- stim generation
+    -----------------------------------------
+    stim_gen_pr:process
+    begin
+
+        enc_data_in    <= (others=>'0');
+        enc_data_in_en <= '0';
+        wait for 500 us;
+        wait until rising_edge(clk);
+
+        for I in 0 to 512 loop
+            wait until rising_edge(clk);
+            enc_data_in    <= x"FF";
+            enc_data_in_en <= '1';
+            wait until rising_edge(clk);
+         
+            enc_data_in_en <= '0';
+         
+            wait until rising_edge(clk);
+            wait until rising_edge(clk);
+            wait until rising_edge(clk);
+            wait until rising_edge(clk);
+         
+        end loop;
+
+        wait;
+
+    end process;
+    
+    -- Loopback inside same module
+    dec_data_in     <= enc_data_out;
+    dec_data_in_en  <= enc_data_out_en;
+
+    -- Encode and decode inside the same module
+    pn_enc_dec_inst : entity work.pn_enc_dec
+    generic map(
+        bit_size =>bit_size
+        )
+    port map
+        (
+        clk => clk,
+        rst_n => rst_n,
+        enc_data_in => enc_data_in,
+        enc_data_in_en => enc_data_in_en,
+        enc_data_out => enc_data_out,
+        enc_data_out_en => enc_data_out_en,
+        dec_data_in => dec_data_in,
+        dec_data_in_en => dec_data_in_en,
+        dec_data_out => dec_data_out,
+        dec_data_out_en => dec_data_out_en
+);
+
+end rtl;
