1a31bd7a042b4ce7d0d69adbdf1087b0e9e34866
gotchicode <fievet.jerome@gmail.com>
2021-07-03 21:30:48 +0200

    modulation - msk modulation update

--- matlab/modulation/test_mod_msk.m
+++ matlab/modulation/test_mod_msk.m
@@ -9,7 +9,7 @@ rand('state',1);
 nb_size=2^12;
 
 %Modulation parameters
-ovr=16; %oversampliing factor 2bit per ovr in MSK
+ovr=8; %oversampliing factor 2bit per ovr in MSK
 msk_deviation=0.5;
 
 %Data generation
@@ -26,6 +26,13 @@ fft_done=20*log10(abs(fftshift(fft(mod_signal))));
 max_fft_done=max(fft_done);
 fft_done=fft_done-max_fft_done;
 
-%Display
+%Display FFT
 figure(1);
 plot(x_axis,fft_done);
+
+%Display signal
+figure(2);
+subplot(211);
+plot(real(mod_signal(1:ovr*16)));
+subplot(212);
+plot(imag(mod_signal(1:ovr*16)));
