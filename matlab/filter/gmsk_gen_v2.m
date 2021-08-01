5e391d4e2b31b360262100b93eaaa0a756598490
gotchicode <fievet.jerome@gmail.com>
2021-08-01 10:11:31 +0200

    modulation - gmsk filter update

--- matlab/filter/gmsk_gen_v2.m
+++ matlab/filter/gmsk_gen_v2.m
@@ -6,7 +6,7 @@ close all;
 BT=0.3; Tb=1; L=8; k=2;
 
 %Impl parameter
-nb_quant=2^7;
+nb_quant=2^15;
 
 %---------------------------------------
 % Ideal
@@ -20,9 +20,9 @@ h_freq = 20*log10(abs(freqz(h,1,512)));
 h_x_axis = (0:0.5/length(h_freq):0.5-0.5/length(h_freq)).';
 
 %---------------------------------------
-% Quant
+% Quant without gain correction
 %---------------------------------------
-max_h = max(h)
+max_h = max(h);
 h_quant = h/max_h;
 h_quant = round(h_quant*(nb_quant-1));
 
@@ -30,6 +30,21 @@ h_quant = round(h_quant*(nb_quant-1));
 h_quant_freq = 20*log10(abs(freqz(h_quant,1,512)));
 h_quant_x_axis = (0:0.5/length(h_quant_freq):0.5-0.5/length(h_quant_freq)).';
 
+%---------------------------------------
+% Quant without with correction
+%---------------------------------------
+fprintf('The gain of the quantified filter is %f\n',10^(max(h_quant_freq)/20));
+fprintf('Closest log 2 gain is %f\n',2^floor(log2(10^(max(h_quant_freq)/20))) );
+fprintf('The ratioi s %f\n', 2^floor(log2(10^(max(h_quant_freq)/20))) /  10^(max(h_quant_freq)/20) );
+ratio_gain = 2^floor(log2(10^(max(h_quant_freq)/20))) /  10^(max(h_quant_freq)/20);
+
+h_quant_comp = h/max_h*ratio_gain;
+h_quant_comp = round(h_quant_comp*(nb_quant-1));
+
+%Work out the reponse of ideal filter
+h_quant_comp_freq = 20*log10(abs(freqz(h_quant_comp,1,512)));
+h_quant_comp_x_axis = (0:0.5/length(h_quant_comp_freq):0.5-0.5/length(h_quant_comp_freq)).';
+fprintf('The gain of the quantified compensated filter is %f\n',10^(max(h_quant_comp_freq)/20));
 %---------------------------------------
 %Display
 %---------------------------------------
