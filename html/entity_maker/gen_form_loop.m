a3e57515f3a57b41ed5e5cadcb07a88c148d9a2a
gotchicode <gotchicode>
2022-09-21 18:03:41 +0200

    entity maker, file added

---
+++ html/entity_maker/gen_form_loop.m
@@ -0,0 +1,39 @@
+clc;
+clear all;
+close all;
+
+fid = fopen("textbox_loop.txt","w");
+
+fprintf(fid,"///////////////////////////\n");
+
+for k=1:20
+
+    fprintf(fid, '<tr>                                                                   \n');
+    fprintf(fid, '  <td>Port_0%d</td>                                                     \n',k);
+    fprintf(fid, '  <td> <input type = "text" id = "Port_0%d_text" /> </td>               \n',k);
+    fprintf(fid, '  <td>                                                                 \n');
+    fprintf(fid, '    <select id = "Port_0%d_dropdown_type">                              \n',k);
+    fprintf(fid, '        <option value = "std_logic" selected>std_logic</option>        \n');
+    fprintf(fid, '        <option value = "std_logic_vector">std_logic_vector</option>   \n');
+    fprintf(fid, '    </select>                                                          \n');
+    fprintf(fid, '   </td>                                                               \n');
+    fprintf(fid, '  <td> <input type = "text" id = "Port_0%d_from_text" size="1"/> </td>  \n',k);
+    fprintf(fid, '  <td> downto </td>                                                    \n');
+    fprintf(fid, '  <td> <input type = "text" id = "Port_0%d_to_text"  size="1"/> </td>   \n',k);
+    fprintf(fid, '  <td> way </td>                                                       \n');
+    fprintf(fid, '  <td>                                                                 \n');
+    fprintf(fid, '    <select id = "Port_0%d_dropdown_way">                               \n',k);
+    fprintf(fid, '        <option value = "IN" selected>IN</option>                      \n');
+    fprintf(fid, '        <option value = "OUT">OUT</option>                             \n');
+    fprintf(fid, '    </select>                                                          \n');
+    fprintf(fid, '  </td>                                                                \n');
+    fprintf(fid, '  <td> used </td>                                                      \n');
+    fprintf(fid, '  <td> <input type = "checkbox" id = "Port_0%d_used" value = "on"> </td>\n',k);
+    fprintf(fid, '</tr>                                                                  \n');
+    fprintf(fid,'\n\n\n');
+  
+end;
+
+fprintf(fid,"///////////////////////////\n");
+
+fclose(fid);
