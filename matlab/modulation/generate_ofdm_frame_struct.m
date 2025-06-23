e1f08a0493b515f1d1b49630b0f1a35f2ec16bc1
jfievet <fievet.jerome@gmail.com>
2025-06-23 17:02:04 +0200

    update of ofdm framing

--- matlab/modulation/generate_ofdm_frame_struct.m
+++ matlab/modulation/generate_ofdm_frame_struct.m
@@ -1,9 +1,13 @@
-function [ofdm_frame_struct,nb_data,nb_gsbcdc,nb_pl]=generate_ofdm_frame_struct(Nfft, Ngsbc, Ndc, Npilotspacing, Npilotspacingsymb2symb, Nsymbperframe)
+function [ofdm_frame_struct,nb_data,nb_gsbcdc,nb_pl]=generate_ofdm_frame_struct(Nfft, Ngsbc, Ndc, Npilotspacing, Npilotspacingsymb2symb, Nsymbperframe,Has_SW ,SW_symbols)
   
-
+ofdm_SW_symbol_struct=ones(SW_symbols,Nfft);
+ofdm_SW_symbol_struct = ones(SW_symbols, Nfft) + 3; % initialize all to 4
+ofdm_SW_symbol_struct(:, Nfft/2) = 0; % DC
+ofdm_SW_symbol_struct(:, 1:Ngsbc/2) = 0; % Guard band (start)
+ofdm_SW_symbol_struct(:, end - Ngsbc/2 + 1:end) = 0; % Guard band (end)
   
 ofdm_symbol_struct=ones(1,Nfft);
-ofdm_symbol_struct(Nfft/2+1)=0; %DC
+ofdm_symbol_struct(Nfft/2)=0; %DC
 ofdm_symbol_struct(1:Ngsbc/2)=0; %Guard subcarriers
 ofdm_symbol_struct(end-Ngsbc/2+1:end)=0; %Guard subcarriers
 
@@ -60,10 +64,60 @@ for k=1:ofdm_frame_struct_size(1)
   end
 end
 
+%Add the SW_symbols
+if Has_SW==1
+  ofdm_frame_struct = [ofdm_SW_symbol_struct; ofdm_frame_struct];
+end
 
-##%Generate an output file
-##fid=fopen('ofdm_frame_struct.html','w');
-##  
-##fclose(fid);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Generation of the ofdm frame in html file
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% Generate an output HTML file
+fid = fopen('ofdm_frame_struct.html', 'w');
+
+fprintf(fid, '<!DOCTYPE html>\n<html>\n<head>\n<title>OFDM Frame Structure</title>\n');
+fprintf(fid, '<style>\n');
+fprintf(fid, 'table { border-collapse: collapse; font-family: monospace; }\n');
+fprintf(fid, 'td, th { border: 1px solid #ccc; text-align: center; width: 25px; height: 25px; }\n');
+fprintf(fid, '.type0 { background-color: #f8d7da; }  /* Guard/DC - red tint */\n');
+fprintf(fid, '.type1 { background-color: #d4edda; }  /* Data - green tint */\n');
+fprintf(fid, '.type2 { background-color: #cce5ff; }  /* Pilot - blue tint */\n');
+fprintf(fid, '.type4 { background-color: #fff3cd; }  /* Sync - yellow tint */\n');
+fprintf(fid, '</style>\n</head>\n<body>\n');
+
+fprintf(fid, '<h2>OFDM Frame Structure</h2>\n');
+
+fprintf(fid, '<p><strong>Legend:</strong></p>\n');
+fprintf(fid, '<ul>\n');
+fprintf(fid, '<li><span style="background-color:#f8d7da; padding: 2px 6px;">0</span> = Guard/DC</li>\n');
+fprintf(fid, '<li><span style="background-color:#d4edda; padding: 2px 6px;">1</span> = Data</li>\n');
+fprintf(fid, '<li><span style="background-color:#cce5ff; padding: 2px 6px;">2</span> = Pilot</li>\n');
+fprintf(fid, '<li><span style="background-color:#fff3cd; padding: 2px 6px;">4</span> = Synchronization Word</li>\n');
+fprintf(fid, '</ul>\n');
+
+fprintf(fid, '<table>\n');
+
+% Header row: subcarrier indices
+fprintf(fid, '<tr><th></th>'); % Empty corner cell
+for subcarrier = 1:Nfft
+    fprintf(fid, '<th>%d</th>', subcarrier);
+end
+fprintf(fid, '</tr>\n');
+
+% Table body
+for symb = 1:size(ofdm_frame_struct, 1)
+    fprintf(fid, '<tr><th>%d</th>', symb); % Symbol number on Y-axis
+    for subc = 1:Nfft
+        val = ofdm_frame_struct(symb, subc);
+        fprintf(fid, '<td class="type%d">%d</td>', val, val);
+    end
+    fprintf(fid, '</tr>\n');
+end
+
+fprintf(fid, '</table>\n</body>\n</html>\n');
+fclose(fid);
+
 
 end
--- matlab/modulation/test_ofdm_find_dc.m
+++ matlab/modulation/test_ofdm_find_dc.m
@@ -4,19 +4,21 @@ close all;
 rand("state",0);
 
 %Parameters
-Nfft=64;
-Ngsbc=2;
+Nfft=32;
+Ngsbc=4;
 Ndc=1;
-Npilotspacing=2;
+Npilotspacing=3;
 Npilotspacingsymb2symb=2;
 Nsymbperframe=10;
+Has_SW=1;
+SW_symbols=4;
 
 %Generate input signal
 data_in_sequence = 2*(round(rand(1,Nfft-Ndc-Ngsbc))-0.5); %-1 because of DC
 data_in_sequence = [0 data_in_sequence]; %First index is DC
 
 %OFDM symbol structure
-[ofdm_frame_struct,nb_data,nb_gsbcdc,nb_pl]=generate_ofdm_frame_struct(Nfft, Ngsbc, Ndc, Npilotspacing, Npilotspacingsymb2symb, Nsymbperframe);
+[ofdm_frame_struct,nb_data,nb_gsbcdc,nb_pl]=generate_ofdm_frame_struct(Nfft, Ngsbc, Ndc, Npilotspacing, Npilotspacingsymb2symb, Nsymbperframe,Has_SW,SW_symbols);
 
 %IFFT to time domain
 ofdm_symb=ifft(data_in_sequence);
@@ -28,12 +30,12 @@ ofdm_symb=ofdm_symb+1/512*2;
 data_recovered=fft(ofdm_symb); %DC=2 in the index 2
 data_recovered_shift=fftshift(data_recovered); %DC=2 goes to Nfft/2+1 index
 
-%Display
-figure(1);
-plot(real(data_in_sequence));
-figure(2);
-plot(real(data_recovered));
-figure(3);
-plot(real(data_recovered_shift));
+##%Display
+##figure(1);
+##plot(real(data_in_sequence));
+##figure(2);
+##plot(real(data_recovered));
+##figure(3);
+##plot(real(data_recovered_shift));
 
 %%On the figure, DC is seen in the position 32
